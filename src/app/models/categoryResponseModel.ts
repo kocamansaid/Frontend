9188b1ba13114905002b2d4426cf727f8a816c7d
Said Kocaman <kocaman631@gmail.com>
2021-03-25 10:16:02 +0300

    1

--- src/app/models/categoryResponseModel.ts
+++ src/app/models/categoryResponseModel.ts
@@ -2,6 +2,6 @@ import { Category } from "./category";
 import { ResponseModel } from "./responseModel";
 
 export interface CategoryResponseModel extends ResponseModel{
-    data:Category[];
+    data:Category[]
     
 }
