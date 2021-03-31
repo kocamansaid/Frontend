8caa0da597cf65551bf00f2ada4317a5c93f4abb
Said Kocaman <kocaman631@gmail.com>
2021-03-31 23:02:51 +0300

    1

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -21,5 +21,9 @@ export class ProductService {
     let newPath=this.apiUrl+"products/getbycategory?categoryId="+categoryId
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
  }
+
+ add(product:Product){
+   return this.httpClient.post(this.apiUrl+"products/add",product);
+ }
 }
 
